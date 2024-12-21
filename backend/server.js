7f06ec85bbe5f63de136411970a8d777fb558d3d
Adi Jain <114348511+Adij003@users.noreply.github.com>
2024-12-21 10:03:27 +0530

    few changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('server...')
+const express = require('express')
+const colors = require('colors')
+
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const app = express()
+
+connectDB()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'welcome to the support desk API'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+// https://github.com/bradtraversy/support-desk
